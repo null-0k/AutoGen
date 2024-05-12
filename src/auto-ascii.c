4f307dea76b66397c576e38680b781b0f26611f6
null-0k <167491690+null-0k@users.noreply.github.com>
2024-05-12 11:17:59 +0900

    Update auto-ascii.c
    Add symbol patterns

--- src/auto-ascii.c
+++ src/auto-ascii.c
@@ -2,18 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <locale.h>
 
 #define SIZE 16
 #define HALF_SIZE (SIZE / 2)
 
 int main () {
-    srand((unsigned int)time(NULL));
+    setlocale(LC_ALL, "");  // ロケールを設定
     char *outputs[SIZE][SIZE]; 
-    char *symbols[3] = {"+", "|"," "};
+    char *symbols[5][3] = {
+        {"＋", "｜", "\xE3\x80\x80"}, // +|(space)
+        {"＋", "ー", "\xE3\x80\x80"}, // +-(space)
+        {"／", "＼", "\xE3\x80\x80"}, // /\(space)
+        {"｜", "ー", "\xE3\x80\x80"}, // |-(space)
+        {"／", "／", "\xE3\x80\x80"}  // //(space)
+    };
 
+    srand((unsigned int)time(NULL));
+    
+    //点対照のパターンを生成
     for (int i = 0; i < HALF_SIZE; i++) {
         for (int j = 0; j < HALF_SIZE; j++) {
-            char *str = symbols[rand() % 3]; 
+            char *str = symbols[rand() % 5][rand() % 3];
             outputs[i][j] = str;
             outputs[i][SIZE - 1 - j] = str;
             outputs[SIZE - 1 - i][j] = str;
@@ -23,10 +33,11 @@ int main () {
 
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            printf("%s ", outputs[i][j]);
+            printf("%s", outputs[i][j]);
         }
         printf("\n");
     }
 
     return 0;
 }
+
